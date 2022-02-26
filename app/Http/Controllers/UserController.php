6c1b404bbcfdae306f825e19b10daea258303669
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-02-26 01:50:19 -0300

    created store for users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class UserController extends Controller
+{
+
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['store']]);
+    }
+
+    public function store(Request $request) {
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $password_confirm = $request->input('password_confirm');
+
+        if(!($name && $email && $password)) {
+            return response()->json([
+                'message' => 'Please provide name, email and password'
+            ], 400);
+        }
+
+        $emailExists = User::where('email', $email)->count();
+
+        if($emailExists) {
+            return response()->json([
+                'message' => 'Email already registred'
+            ], 400);
+        }
+
+        if($password !== $password_confirm) {
+            return response()->json([
+                'message' => 'Passwords do not match'
+            ], 400);
+        }
+
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $user = new User();
+        $user->name = $name;
+        $user->email = $email;
+        $user->password = $hash;
+        $user->save();
+        $user = User::where('email', $email)->first();
+
+        return $user;
+    }
+}
